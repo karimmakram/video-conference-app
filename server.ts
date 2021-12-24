26aa61686066eca76526d2248bcefa7bb96f90cf
karimmakram <karimomakram11@gmail.com>
2021-12-24 09:31:50 +0200

    enable cors

--- server.ts
+++ server.ts
@@ -8,6 +8,16 @@ import { ExpressPeerServer } from 'peer'
 
 const app = express()
 app.use(cors())
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  )
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  next()
+})
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 app.set('view engine', 'ejs')
